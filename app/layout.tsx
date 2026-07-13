f2860ddcab1fa5f84298871674f33f91591c8f5e
Poghos Boyajyan <poghosboyajyan@gmail.com>
2026-07-14 01:09:55 +0400

    gtm inject

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,11 @@ import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 import { Suspense } from "react"
 import "./globals.css"
+
+const GTM_ID = "GTM-M2B5VTBW"
 
 export const metadata: Metadata = {
   title: "Autobanali.am - Բանալիների վերականգնում և կրկնօրինակում",
@@ -35,7 +38,32 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+            id="gtm-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
       <Suspense fallback={null}>{children}</Suspense>
       <Analytics />
       </body>
